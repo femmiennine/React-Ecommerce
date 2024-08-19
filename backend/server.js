279a60e65d5be19a4b2ca48b1e90ab4ebdbb06e6
Frances Fogarty <53113092+femmiennine@users.noreply.github.com>
2024-08-19 16:53:08 +0200

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoute from './routes/productRoute.js';
+import userRoute from './routes/userRoute.js';
+import orderRoute from './routes/orderRoute.js';
+import uploadRoute from './routes/uploadRoute.js';
+
+const port = process.env.PORT || 8000;
+
+connectDB();
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cokkie paser middleware
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to eCommerce API!');
+});
+
+app.use('/api/products', productRoute);
+app.use('/api/users', userRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/upload', uploadRoute);
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
+const __dirname = path.resolve(); // Set __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server running on port http://localhost:${port}`)
+);
